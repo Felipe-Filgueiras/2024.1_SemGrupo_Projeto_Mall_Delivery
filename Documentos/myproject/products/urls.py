00da8c60f0f945c94c1d78512cc0ea984ac8cdd6
Felipe-Filgueiras <116523039+Felipe-Filgueiras@users.noreply.github.com>
2024-06-14 00:23:18 -0300

    del

--- Documentos/myproject/products/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.product_list, name='product_list'),
-    path('add/', views.product_add, name='product_add'),
-    path('<int:pk>/delete/', views.product_delete, name='product_delete'),
-]
