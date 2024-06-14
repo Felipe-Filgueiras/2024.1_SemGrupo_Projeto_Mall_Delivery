327439ea16bf71c18e3b15d5b8d12d439f9107d8
Felipe-Filgueiras <116523039+Felipe-Filgueiras@users.noreply.github.com>
2024-06-14 09:48:59 -0300

    correção

--- ProjetoDjango/myproject/products/forms.py
+++ ProjetoDjango/myproject/products/forms.py
@@ -6,6 +6,13 @@ class ProductForm(forms.ModelForm):
     class Meta:
         model = Product
         fields = ['name', 'description', 'price', 'category', 'image']
+        labels = {
+            'name': 'Nome',  # Change these as per your language or preference
+            'description': 'Descrição',
+            'price': 'Preço',
+            'category': 'Categoria',
+            'image': 'Imagem'
+        }
 
 class UserProfileForm(forms.ModelForm):
     class Meta:
